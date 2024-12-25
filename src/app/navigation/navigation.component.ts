ce5caee8c0798c159b3a366bb1d530c01dff1c40
my school <schoolm913@gmail.com>
2024-12-25 13:47:57 +0530

    sidebar1

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, tap } from 'rxjs/operators';
 import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
@@ -16,6 +16,7 @@ export class NavigationComponent {
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
+      tap(matches => console.log('Breakpoint Handset matches:', matches)),
       shareReplay()
     );
 }
