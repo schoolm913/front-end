0818a77ab57ffe61934112ef4938fef83dc97c74
my school <schoolm913@gmail.com>
2024-12-26 21:32:40 +0530

    changes

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal, Signal, WritableSignal } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay, tap } from 'rxjs/operators';
@@ -12,6 +12,20 @@ import { AsyncPipe, NgIf } from '@angular/common';
 })
 export class NavigationComponent {
   private breakpointObserver = inject(BreakpointObserver);
+  public collabseSideBar: WritableSignal<boolean> = signal(false);
+  public collabseSideBarWidth: Signal<string> = computed(() => this.collabseSideBar() ? '64px' : '250px');
+  public menu = [
+    {
+      name: 'Home',
+      icon: 'home',
+      subMenu: [
+        {
+          name: 'Dashboard',
+          icon: 'home'
+        }
+      ]
+    }
+  ]
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -19,4 +33,13 @@ export class NavigationComponent {
       tap(matches => console.log('Breakpoint Handset matches:', matches)),
       shareReplay()
     );
+
+    public onSidenavOpenedChange(event: any){
+
+    }
+
+    public onSidenavClosedStart(){
+      
+    }
+
 }
